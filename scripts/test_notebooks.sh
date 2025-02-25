170a339abcf41cb5a2fd38932b7d1e32d839ab1c
Brian Kroth <bpkroth@microsoft.com>
2025-02-25 15:23:29 -0600

    add CI support

---
+++ scripts/test_notebooks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is a simple script used to test the repo in the CI pipeline.
+
+set -eu
+
+# Move to repo root.
+scriptdir=$(dirname "$(readlink -f "$0")")
+cd "$scriptdir/.."
+
+set -x
+
+# Run the jupyter notebooks.
+find . -maxdepth 2 -name '*.ipynb' -print0 | xargs -t -0 -P0 -n1 conda run -n mlos jupyter execute
+
+echo "OK"
+exit 0
