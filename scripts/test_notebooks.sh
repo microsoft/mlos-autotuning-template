eba0c23a7fcb61c7796290826e21c2b5b7a5499b
Brian Kroth <bpkroth@microsoft.com>
2025-02-25 15:54:30 -0600

    accept no notebooks to run

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -11,7 +11,7 @@ cd "$scriptdir/.."
 set -x
 
 # Run the jupyter notebooks.
-find . -maxdepth 2 -name '*.ipynb' -print0 | xargs -t -0 -P0 -n1 conda run -n mlos jupyter execute
+find . -maxdepth 2 -name '*.ipynb' -print0 | xargs -r -t -0 -P0 -n1 conda run -n mlos jupyter execute
 
 echo "OK"
 exit 0
